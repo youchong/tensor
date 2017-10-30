81a265f016fbc630e9cacc935e362f23f668c2a4
you <956282766@qq.com>
2017-10-30 18:45:43 +0800

    2 layers with problems

--- SoftmaxRegressionMLP.py
+++ SoftmaxRegressionMLP.py
@@ -5,18 +5,22 @@ mnist=input_data.read_data_sets("MNIST_data/",one_hot=True)
 sess=tf.InteractiveSession()
 
 in_units=784
-h1_units=300
+h1_units=500
+h2_units=300
 W1=tf.Variable(tf.truncated_normal([in_units,h1_units],stddev=0.1))
 b1=tf.Variable(tf.zeros([h1_units]))
-W2=tf.Variable(tf.zeros([h1_units,10]))
-b2=tf.Variable(tf.zeros([10]))
+W2=tf.Variable(tf.zeros([h1_units,h2_units]))
+b2=tf.Variable(tf.zeros([h2_units]))
+W3=tf.Variable(tf.zeros([h2_units,10]))
+b3=tf.Variable(tf.zeros([10]))
 
 x=tf.placeholder(tf.float32,[None,in_units])
 keep_prob=tf.placeholder(tf.float32)
 
 hidden1=tf.nn.relu(tf.matmul(x,W1)+b1)
 hidden1_drop=tf.nn.dropout(hidden1,keep_prob)
-y=tf.nn.softmax(tf.matmul(hidden1_drop,W2)+b2)
+hidden2=tf.nn.relu(tf.matmul(hidden1_drop,W2)+b2)
+y=tf.nn.softmax(tf.matmul(hidden2,W3)+b3)
 
 y_=tf.placeholder(tf.float32,[None,10])
 cross_entropy=tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y),reduction_indices=[1]))
