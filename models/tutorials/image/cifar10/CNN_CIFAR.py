629cc41be46a3241efa3ffe2004a78f1a53bbec2
you <956282766@qq.com>
2017-11-06 19:53:57 +0800

    cifar finish

--- models/tutorials/image/cifar10/CNN_CIFAR.py
+++ models/tutorials/image/cifar10/CNN_CIFAR.py
@@ -32,9 +32,68 @@ pool1=tf.nn.max_pool(conv1,ksize=[1,3,3,1],strides=[1,2,2,1],padding='SAME')
 norm1=tf.nn.lrn(pool1,4,bias=1.0,alpha=0.001/9.0,beta=0.75)
 
 # C2
-weight2=variable_with_weight_loss(shape=[5,5,64,64],stddv=5e-2,wl=0.0)
-kernel2=tf.nn.con2d(norm1,weight2,[1,1,1,1],padding='SAME')
+weight2=variable_with_weight_loss(shape=[5,5,64,64],stddev=5e-2,wl=0.0)
+kernel2=tf.nn.conv2d(norm1,weight2,[1,1,1,1],padding='SAME')
 bias2=tf.Variable(tf.constant(0.1,shape=[64]))
-conv2=tf.nn.rellu(tf.nn.bias_add(kernel2,bias2))
+conv2=tf.nn.relu(tf.nn.bias_add(kernel2,bias2))
 norm2=tf.nn.lrn(conv2,4,bias=1.0,alpha=0.001/9.0,beta=0.75)
 pool2=tf.nn.max_pool(norm2,ksize=[1,3,3,1],strides=[1,2,2,1],padding='SAME')
+
+# F1
+reshape=tf.reshape(pool2,[batch_size,-1])
+dim=reshape.get_shape()[1].value
+weight3=variable_with_weight_loss(shape=[dim,384],stddev=0.04,wl=0.004)
+bias3=tf.Variable(tf.constant(0.1,shape=[384]))
+local3=tf.nn.relu(tf.matmul(reshape,weight3)+bias3)
+
+#F2
+weight4=variable_with_weight_loss(shape=[384,192],stddev=0.04,wl=0.004)
+bias4=tf.Variable(tf.constant(0.1,shape=[192]))
+local4=tf.nn.relu(tf.matmul(local3,weight4)+bias4)
+
+weight5=variable_with_weight_loss(shape=[192,10],stddev=1/192.0,wl=0.0)
+bias5=tf.Variable(tf.constant(0.0,shape=[10]))
+logits=tf.add(tf.matmul(local4,weight5),bias5)
+
+def loss(logits,labels):
+	labels=tf.cast(labels,tf.int64)
+	cross_entropy=tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=labels,name='cross_entropy_per_example')
+	cross_entropy_mean=tf.reduce_mean(cross_entropy,name='cross_entropy')
+	tf.add_to_collection('losses',cross_entropy_mean)
+	return tf.add_n(tf.get_collection('losses'),name='total_loss')
+
+loss=loss(logits,label_holder)
+train_op=tf.train.AdamOptimizer(1e-3).minimize(loss)
+top_k_op=tf.nn.in_top_k(logits,label_holder,1)
+sess=tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+tf.train.start_queue_runners()
+
+for step in range(max_steps):
+	start_time=time.time()
+	image_batch,label_batch=sess.run([images_train,labels_train])
+	_temp,loss_value=sess.run([train_op,loss],feed_dict={image_holder:image_batch,label_holder:label_batch})
+	duration=time.time()-start_time
+	if step%10==0:
+		examples_per_sec=batch_size/duration
+		sec_per_batch=float(duration)
+		format_str=('step %d,loss%.2f(%.1f examples/sec;%.3f sec/batch)')
+		print(format_str%(step,loss_value,examples_per_sec,sec_per_batch))
+
+num_examples=10000
+import math
+num_iter=int(math.ceil(num_examples/batch_size))
+true_count=0
+total_sample_count=num_iter*batch_size
+step=0
+total_sample_count=num_iter*batch_size
+step=0
+while step<num_iter:
+	image_batch,label_batch=sess.run([images_test,labels_test])
+	predictions=sess.run([top_k_op],feed_dict={image_holder:image_batch,label_holder:label_batch})
+	true_count+=np.sum(predictions)
+	step+=1
+
+precision=true_count/total_sample_count
+print('precision@1=%.3f'%precision)
